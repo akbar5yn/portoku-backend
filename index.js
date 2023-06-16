ca99ebbc673d7580e36fa7bf8fdef365f4bf0834
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-17 03:22:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const connectDb = require("./db/dbConnect");
+const cors = require("cors");
+connectDb();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+const router = express.Router();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: false,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/", (req, res) => {
+//   res.json({ message: "server successful" });
+// });
+
+app.use("/users", require("./app/routes/userRoutes"));
+
+app.listen(port, () => {
+  console.log(`server started at port http://localhost:${port}/`);
+});
+
+app.use("/.netlify/functions/index", router);
+
+module.exports.handler = serverless(app);
